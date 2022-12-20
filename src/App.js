5a53ea92ca00f2f905c1f1bc6d9c15433f7133f2
Badal Narnaware <badal.p.n@gmail.com>
2022-12-20 16:26:24 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <img src="https://picsum.photos/400" alt="" />
+      <h1>Person Name </h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea ab ratione
+        autem recusandae qui nemo maxime voluptatibus numquam quia error.
+        Repellat porro perferendis autem itaque perspiciatis, corrupti deleniti
+        maiores praesentium?
+      </p>
     </div>
   );
 }
